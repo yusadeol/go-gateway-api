56e42abe6c451a74a541c9c10375900fca3fc003
Yuri Oliveira <yusadeol@gmail.com>
2025-04-11 15:04:57 -0300

    feat: implement domain model and repository for invoices
    - Added `Invoice` and `CreditCard` structs along with business logic
    in the domain layer
    - Introduced `Status` type with values: pending, approved, and rejected
    - Implemented `InvoiceRepository` with methods to save, find, and
    update invoices using SQL
    - Defined `InvoiceRepository` interface in the domain for abstraction

---
+++ internal/domain/invoice.go
@@ -0,0 +1,86 @@
+package domain
+
+import (
+	"github.com/google/uuid"
+	"math/rand"
+	"time"
+)
+
+type Status string
+
+const (
+	StatusPending  Status = "pending"
+	StatusApproved Status = "approved"
+	StatusRejected Status = "rejected"
+)
+
+type Invoice struct {
+	ID             string
+	AccountID      string
+	Status         Status
+	Description    string
+	PaymentType    string
+	Amount         float64
+	CardLastDigits string
+	CreatedAt      time.Time
+	UpdatedAt      time.Time
+}
+
+type CreditCard struct {
+	Number         string
+	CVV            string
+	ExpiryMonth    int
+	ExpiryYear     int
+	CardHolderName string
+}
+
+func NewInvoice(
+	accountID string,
+	amount float64,
+	description string,
+	paymentType string,
+	card CreditCard,
+) (*Invoice, error) {
+	if amount <= 0 {
+		return nil, ErrInvalidAmount
+	}
+
+	cardLastDigits := card.Number[len(card.Number)-4:]
+
+	return &Invoice{
+		ID:             uuid.New().String(),
+		AccountID:      accountID,
+		Status:         StatusPending,
+		Description:    description,
+		PaymentType:    paymentType,
+		Amount:         amount,
+		CardLastDigits: cardLastDigits,
+		CreatedAt:      time.Now(),
+		UpdatedAt:      time.Now(),
+	}, nil
+}
+
+func (i *Invoice) Process() {
+	if i.Amount > 10000 {
+		return
+	}
+
+	randomSource := rand.New(rand.NewSource(time.Now().Unix()))
+
+	if randomSource.Float64() > 0.7 {
+		i.Status = StatusRejected
+		return
+	}
+
+	i.Status = StatusApproved
+}
+
+func (i *Invoice) UpdateStatus(status Status) error {
+	if status == StatusPending {
+		return ErrInvalidStatus
+	}
+
+	i.Status = status
+	i.UpdatedAt = time.Now()
+	return nil
+}
---
+++ internal/repository/invoice_repository.go
@@ -0,0 +1,142 @@
+package repository
+
+import (
+	"database/sql"
+	"errors"
+	"github.com/yusadeol/go-gateway-api/internal/domain"
+	"time"
+)
+
+type InvoiceRepository struct {
+	db *sql.DB
+}
+
+func NewInvoiceRepository(db *sql.DB) *InvoiceRepository {
+	return &InvoiceRepository{db: db}
+}
+
+func (i *InvoiceRepository) Save(invoice *domain.Invoice) error {
+	stmt, err := i.db.Prepare(`
+		INSERT INTO invoices (id, account_id, status, description, payment_type, amount, card_last_digits, created_at, updated_at)
+		VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
+	`)
+
+	if err != nil {
+		return err
+	}
+
+	defer stmt.Close()
+
+	_, err = stmt.Exec(
+		invoice.ID,
+		invoice.AccountID,
+		invoice.Status,
+		invoice.Description,
+		invoice.PaymentType,
+		invoice.Amount,
+		invoice.CardLastDigits,
+		invoice.CreatedAt,
+		invoice.UpdatedAt,
+	)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (i *InvoiceRepository) FindByID(id string) (*domain.Invoice, error) {
+	var invoice domain.Invoice
+
+	err := i.db.QueryRow(
+		`SELECT * FROM invoices WHERE id = ?`,
+		id,
+	).Scan(
+		&invoice.ID,
+		&invoice.AccountID,
+		&invoice.Status,
+		&invoice.Description,
+		&invoice.PaymentType,
+		&invoice.Amount,
+		&invoice.CardLastDigits,
+		&invoice.CreatedAt,
+		&invoice.UpdatedAt,
+	)
+
+	if errors.Is(err, sql.ErrNoRows) {
+		return nil, domain.ErrInvoiceNotFound
+	}
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &invoice, nil
+}
+
+func (i *InvoiceRepository) FindByAccountID(accountID string) ([]*domain.Invoice, error) {
+	rows, err := i.db.Query(
+		`SELECT * FROM invoices WHERE account_id = ?`,
+		accountID,
+	)
+
+	if errors.Is(err, sql.ErrNoRows) {
+		return nil, domain.ErrInvoiceNotFound
+	}
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	var invoices []*domain.Invoice
+	for rows.Next() {
+		var invoice domain.Invoice
+		err := rows.Scan(
+			&invoice.ID,
+			&invoice.AccountID,
+			&invoice.Status,
+			&invoice.Description,
+			&invoice.PaymentType,
+			&invoice.Amount,
+			&invoice.CardLastDigits,
+			&invoice.CreatedAt,
+			&invoice.UpdatedAt,
+		)
+
+		if err != nil {
+			return nil, err
+		}
+
+		invoices = append(invoices, &invoice)
+	}
+
+	return invoices, nil
+}
+
+func (i *InvoiceRepository) UpdateStatus(invoice *domain.Invoice) error {
+	var res sql.Result
+	res, err := i.db.Exec(
+		`UPDATE invoices SET status = ?, updated_at = ? WHERE id = ?`,
+		invoice.Status,
+		time.Now(),
+		invoice.ID,
+	)
+
+	if err != nil {
+		return err
+	}
+
+	var affectedRows int64
+	affectedRows, err = res.RowsAffected()
+	if err != nil {
+		return err
+	}
+
+	if affectedRows == 0 {
+		return domain.ErrInvoiceNotFound
+	}
+
+	return nil
+}
